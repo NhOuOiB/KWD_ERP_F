1125b58a04ee100cabb03f9cf1676042bf9e7c77
NhOuOiB <s33165309000@gmail.com>
2023-04-21 17:58:12 +0800

    [feat] leave takeLeave route

--- src/component/HR/HR.jsx
+++ src/component/HR/HR.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import { BrowserRouter, Routes, Route, Router } from 'react-router-dom';
 import AddEmployee from './AddEmployee/AddEmployee';
 import Employee from './Employee/Employee';
+import Leave from './Leave/Leave';
+import AddLeave from './AddLeave/AddLeave';
 import auth from '../../auth/auth';
 
 const HR = () => {
@@ -9,10 +11,12 @@ const HR = () => {
 
   return (
     <Routes>
-      <Route path='/employee' element={<Employee />} />
-      <Route path='/addEmployee' element={<AddEmployee />} />
+      <Route path="/employee" element={<Employee />} />
+      <Route path="/addEmployee" element={<AddEmployee />} />
+      <Route path="/leave" element={<Leave />} />
+      <Route path="/addLeave" element={<AddLeave />} />
     </Routes>
   );
 };
 
-export default HR;
+export default HR;  

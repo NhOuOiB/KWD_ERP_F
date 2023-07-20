f36df42e097662bc195583b68b639c6f7a3eea35
mike.chang <s33165309000@gmail.com>
2023-07-20 14:57:57 +0800

    [feat] layout adjustment

--- src/component/HR/Leave/Leave.jsx
+++ src/component/HR/Leave/Leave.jsx
@@ -1,11 +1,9 @@
 import axios from 'axios';
-import auth from '../../../auth/auth';
 import { useEffect, useState } from 'react';
 import { API_URL } from '../../../utils/config';
 import moment from 'moment/moment';
 
 const Leave = () => {
-  auth();
   const [leave, setLeave] = useState([]);
 
   useEffect(() => {
@@ -16,43 +14,60 @@ const Leave = () => {
   }, []);
   return (
     <div className="h-full flex flex-col justify-center items-center ">
-      <div className="">
-        <table className="table-auto shadow shadow-gray-600">
+      <div className="h-[47.3rem] overflow-auto">
+        <table className="table-auto shadow shadow-gray-600 h-32 overflow-auto">
           <thead>
-            <tr>
+            <tr className='border border-[#242424]'>
+              <th>申請時間</th>
               <th className="p-2">休假日期</th>
               <th className="p-2">職員</th>
               <th className="p-2">休假項目</th>
               <th className="p-2">休假時數</th>
               <th className="p-2">內容</th>
-              <th>申請時間</th>
             </tr>
           </thead>
           <tbody className="bg-white">
             {leave.map((v, i) => {
               return (
-                <tr className="text-[#444] font-bold" key={i}>
-                  <td className="border">
-                    <div className="border w-96 py-2">
-                      {v.begin} ~ {v.end}
-                    </div>
-                  </td>
-                  <td className="border">
-                    <div className="border w-40 py-2">{v.name}</div>
-                  </td>
-                  <td className="border ">
-                    <div className="border w-40 py-2">{v.leave_name}</div>
-                  </td>
-                  <td className="border ">
-                    <div className="border w-40 py-2">{v.hour}</div>
-                  </td>
-                  <td className="border ">
-                    <div className="border w-40 h-[37px] py-2">{v.note}</div>
-                  </td>
-                  <td className="border">
-                    <div className="border w-40 py-2">{moment(v.time).format('YYYY-MM-DD')}</div>
-                  </td>
-                </tr>
+                <>
+                  {i === 0 ? (
+                    <tr className="text-[#444] font-bold text-xl border">
+                      <td colSpan={6} className='py-4'>{leave[i + 1].end.slice(0, 4)}</td>
+                    </tr>
+                  ) : (
+                    ''
+                  )}
+                  <tr className="text-[#444] font-bold" key={i}>
+                    <td className="border">
+                      <div className="border w-40 py-2">{moment(v.time).format('YYYY-MM-DD')}</div>
+                    </td>
+                    <td className="border">
+                      <div className="border w-96 py-2">
+                        {moment(v.begin).format('MM 月 DD 日 HH 點')} ~ {moment(v.end).format('MM 月 DD 日 HH 點')}
+                      </div>
+                    </td>
+                    <td className="border">
+                      <div className="border w-32 py-2">{v.name}</div>
+                    </td>
+                    <td className="border ">
+                      <div className="border w-32 py-2">{v.leave_name}</div>
+                    </td>
+                    <td className="border ">
+                    
+                      <div className="border w-32 py-2">{v.hour}</div>
+                    </td>
+                    <td className="border ">
+                      <div className="border w-32 h-[37px] py-2">{v.note}</div>
+                    </td>
+                  </tr>
+                  {i < leave.length - 1 && leave[i].end.slice(0, 4) != leave[i + 1].end.slice(0, 4) ? (
+                    <tr className="text-[#444] font-bold text-xl border">
+                      <td colSpan={6} className='py-4'>{leave[i + 1].end.slice(0, 4)}</td>
+                    </tr>
+                  ) : (
+                    ''
+                  )}
+                </>
               );
             })}
           </tbody>

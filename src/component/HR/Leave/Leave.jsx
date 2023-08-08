341d54f8d935e6621618d1eb72fa76334992ac67
mike.chang <s33165309000@gmail.com>
2023-08-08 17:48:46 +0800

    [fix] key

--- src/component/HR/Leave/Leave.jsx
+++ src/component/HR/Leave/Leave.jsx
@@ -1,7 +1,8 @@
+import React from 'react';
 import axios from 'axios';
+import moment from 'moment/moment';
 import { useEffect, useState } from 'react';
 import { API_URL } from '../../../utils/config';
-import moment from 'moment/moment';
 
 const Leave = () => {
   const [leave, setLeave] = useState([]);
@@ -17,7 +18,7 @@ const Leave = () => {
       <div className="h-[47.3rem] overflow-auto">
         <table className="table-auto shadow shadow-gray-600 h-32 overflow-auto">
           <thead>
-            <tr className='border border-[#242424]'>
+            <tr className="border border-[#242424]">
               <th>申請時間</th>
               <th className="p-2">休假日期</th>
               <th className="p-2">職員</th>
@@ -29,15 +30,17 @@ const Leave = () => {
           <tbody className="bg-white">
             {leave.map((v, i) => {
               return (
-                <>
+                <React.Fragment key={v.id}>
                   {i === 0 ? (
                     <tr className="text-[#444] font-bold text-xl border">
-                      <td colSpan={6} className='py-4'>{leave[i + 1].end.slice(0, 4)}</td>
+                      <td colSpan={6} className="py-4">
+                        {leave[i + 1].end.slice(0, 4)}
+                      </td>
                     </tr>
                   ) : (
                     ''
                   )}
-                  <tr className="text-[#444] font-bold" key={i}>
+                  <tr className="text-[#444] font-bold">
                     <td className="border">
                       <div className="border w-40 py-2">{moment(v.time).format('YYYY-MM-DD')}</div>
                     </td>
@@ -53,21 +56,22 @@ const Leave = () => {
                       <div className="border w-32 py-2">{v.leave_name}</div>
                     </td>
                     <td className="border ">
-                    
                       <div className="border w-32 py-2">{v.hour}</div>
                     </td>
                     <td className="border ">
                       <div className="border w-32 h-[37px] py-2">{v.note}</div>
                     </td>
                   </tr>
-                  {i < leave.length - 1 && leave[i].end.slice(0, 4) != leave[i + 1].end.slice(0, 4) ? (
+                  {i < leave.length - 1 && leave[i].end.slice(0, 4) !== leave[i + 1].end.slice(0, 4) ? (
                     <tr className="text-[#444] font-bold text-xl border">
-                      <td colSpan={6} className='py-4'>{leave[i + 1].end.slice(0, 4)}</td>
+                      <td colSpan={6} className="py-4">
+                        {leave[i + 1].end.slice(0, 4)}
+                      </td>
                     </tr>
                   ) : (
                     ''
                   )}
-                </>
+                </React.Fragment>
               );
             })}
           </tbody>

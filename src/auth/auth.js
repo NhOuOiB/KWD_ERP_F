17263cb4e91d2887811a8c3c33f614021833727e
mike.chang <s33165309000@gmail.com>
2024-01-10 10:14:41 +0800

    [feat] UserContext

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -1,17 +1,23 @@
 import axios from 'axios';
-import { useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { API_URL } from '../utils/config';
 import { toast } from 'react-toastify';
+import UserContext from '../store/userContext';
 
-const userPermission = () => {
+const userPermission = (history) => {
   const navigate = useNavigate();
+  const { user, setUser } = useContext(UserContext);
+  const { id, name, permission } = user;
+
   useEffect(() => {
     (async () => {
       try {
         let result = await axios.get(`${API_URL}/auth`, {
           withCredentials: true,
         });
+        const { id, name, permission } = result.data;
+        setUser({ id: id, name: name, permission: permission, history: history });
       } catch (err) {
         toast.error(err.response.data.message, {
           position: 'top-center',
@@ -22,10 +28,11 @@ const userPermission = () => {
           draggable: true,
           theme: 'dark',
         });
+        setUser({ id: id, name: name, permission: permission, history: history });
         if (err.response.status == 401) navigate('/');
       }
     })();
-  }, []);
+  }, [history]);
 };
 
 export default userPermission;

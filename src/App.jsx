39ab2dbcfcbdaec3701a964927d06291c3f409de
mike.chang <s33165309000@gmail.com>
2024-01-10 10:14:19 +0800

    [feat] UserContext

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,23 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
 import Login from './component/Login/Login';
 import HR from './component/HR/HR';
+import ClockIn from './component/ClockIn/ClockIn';
 import Header from './component/Common/Header';
+import { UserContextProvider } from './store/userContext';
 
 function App() {
   return (
-    <BrowserRouter initialEntries={['/']} initialIndex={0} basename=''>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Login />}></Route>
-        <Route path="/HR/*" element={<HR />}></Route>
-      </Routes>
-      <ToastContainer />
-    </BrowserRouter>
+    <UserContextProvider>
+      <BrowserRouter initialEntries={['/']} initialIndex={0} basename="">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Login />}></Route>
+          <Route path="/HR/*" element={<HR />}></Route>
+          <Route path="/clock" element={<ClockIn />}></Route>
+        </Routes>
+        <ToastContainer />
+      </BrowserRouter>
+    </UserContextProvider>
   );
 }
 

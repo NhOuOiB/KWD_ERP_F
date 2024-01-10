def96bcb17ffb45a8891b22b1fb97c07e7970ab6
mike.chang <s33165309000@gmail.com>
2024-01-10 10:16:42 +0800

    [feat] ClockIn

--- src/component/Common/Header.jsx
+++ src/component/Common/Header.jsx
@@ -1,17 +1,22 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../../styles/Common/_header.scss';
 import { Menu } from 'antd';
 import { FaUser } from 'react-icons/fa';
 import { AiOutlineUser } from 'react-icons/ai';
 import { HiOutlineDocument } from 'react-icons/hi';
+import { FaRegClock } from 'react-icons/fa6';
 import logo from '/images.jpg';
+import auth from '../../auth/auth';
+import UserContext from '../../store/userContext';
 
 const Header = () => {
   const [current, setCurrent] = useState('mail');
   const onClick = (e) => {
     setCurrent(e.key);
   };
+  const { user } = useContext(UserContext)
+  const { id, name, permission } = user
   const items = [
     {
       label: <img src={logo} className="w-14 border-none"></img>,
@@ -71,18 +76,34 @@ const Header = () => {
       icon: <HiOutlineDocument />,
       children: [
         {
-          label: <Link to={''}>查看產品</Link>,
+          label: <Link to={'/pro/product'}>查看產品</Link>,
           key: 'product',
         },
         {
-          label: <Link to={''}>新增產品</Link>,
+          label: <Link to={'/pro/addProduct'}>新增產品</Link>,
           key: 'addProduct',
         },
       ],
     },
+    {
+      label: <Link to={'/clock'}>打卡</Link>,
+      key: 'clock',
+      icon: <FaRegClock />,
+    },
   ];
 
-  return <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} />;
+  const filteredItems = items.filter((item) => {
+    if (id === 1) {
+
+    } else if (item.key === 'pro' && permission === 2) {
+      return false;
+    } else if ((item.key === 'hr' || item.key === 'pro') && permission === 1) {
+      return false;
+    } 
+    return true; 
+  });
+
+  return <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={filteredItems} />;
 };
 
 export default Header;

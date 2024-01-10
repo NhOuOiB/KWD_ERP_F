cf1753bdc4583d590a2e7162835d7c4a730facd4
mike.chang <s33165309000@gmail.com>
2024-01-10 10:17:32 +0800

    [feat] permission

--- src/component/HR/HR.jsx
+++ src/component/HR/HR.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import Leave from './Leave/Leave';
 import auth from '../../auth/auth';
 import Salary from './Salary/Salary';
@@ -7,18 +7,27 @@ import Employee from './Employee/Employee';
 import LeaveDays from './LeaveDays/LeaveDays';
 import AddEmployee from './AddEmployee/AddEmployee';
 import AddSalary from './Salary/AddSalary';
+import { useContext } from 'react';
+import UserContext from '../../store/userContext';
 
 const HR = () => {
-  auth();
+  const location = useLocation();
+  auth(location.pathname);
+  const { user } = useContext(UserContext);
+  const { id, permission } = user;
   return (
     <Routes>
-      <Route path="/employee" element={<Employee />} />
-      <Route path="/addEmployee" element={<AddEmployee />} />
-      <Route path="/leave" element={<Leave />} />
-      <Route path="/addLeave" element={<AddLeave />} />
-      <Route path="/leaveDays" element={<LeaveDays />} />
-      <Route path="/salary" element={<Salary />} />
-      <Route path="/addSalary" element={<AddSalary />} />
+      {permission === 2 || id === 1 && (
+        <>
+          <Route path="/employee" element={<Employee />} />
+          <Route path="/addEmployee" element={<AddEmployee />} />
+          <Route path="/leave" element={<Leave />} />
+          <Route path="/addLeave" element={<AddLeave />} />
+          <Route path="/leaveDays" element={<LeaveDays />} />
+          <Route path="/salary" element={<Salary />} />
+          <Route path="/addSalary" element={<AddSalary />} />
+        </>
+      )}
     </Routes>
   );
 };
